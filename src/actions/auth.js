4cebdd5d9bed9c2cb028ff8c56a62eda2fc154a0
Rebecca Crow <rdcrow529@gmail.com>
2019-01-21 12:14:07 -0500

    test login error

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -65,6 +65,7 @@ export const login = (username, password) => dispatch => {
                     code === 401
                         ? 'Incorrect username or password'
                         : 'Unable to login, please try again';
+                console.log(err)
                 dispatch(authError(err));
                
                 return Promise.reject(
